edd594761c260ff748e6d3ba8cfa3b98c223ed37
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-16 22:00:42 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,7 +3,7 @@ module Findable
   module ClassMethods
     
     def find_by_name(name)
-      self.all.detect{|a| a.name == name}
+      self.all.detect{|a| a.name == name}https://learn.co/slack
     end
   end
   
